0888ed42422eb735aad30b2c16a07d8595bc41de
Marcos Alves <102601006+devmarcosalves@users.noreply.github.com>
2022-10-14 20:50:09 -0300

    twelveth commit
    Correção do "header" no script.js;
    Aplicação da tag semântica footer e atribuição de texto dos direitos reservados;
    Definição de alinhamento de texto, afastamento interno, tamanho de fonte e cor.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const header = document.querySelector("hearder");
+const header = document.querySelector("header");
 
 window.addEventListener("scroll", function(){
 	header.classList.toggle("sticky", window.scrollY > 0);
