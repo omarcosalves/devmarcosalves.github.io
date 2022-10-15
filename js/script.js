4cd649c5150109efe082b301ebaaf02c45df8c3d
Marcos Alves <102601006+devmarcosalves@users.noreply.github.com>
2022-10-14 21:51:49 -0300

    fifteenth commit
    Aplicação de efeito de entrada suave.

--- js/script.js
+++ js/script.js
@@ -9,3 +9,10 @@ menu.onlick = () => {
 	menu.classList.toggle('bx-x');
 	navlist.classList.toggle('active');
 }
+const sr = ScrollReveal({
+	distance: '50px',
+	duration:2500,
+	reset: true
+})
+
+sr.reveal('.home-text',{delay:350, origin:'left'})
