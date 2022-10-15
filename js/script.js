f06942f6456a9ec21035ec5f96e3edf0780b5001
Marcos Alves <102601006+devmarcosalves@users.noreply.github.com>
2022-10-14 21:31:14 -0300

    fourteenth commit
    Aplicação das dinâmicas para acessar o menu sanduíche.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
 	header.classList.toggle("sticky", window.scrollY > 0);
 });
+let menu = document.querySelector('#navbar-icon');
+let navlist = document.querySelector('.navbar-header');
+menu.onlick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('active');
+}
